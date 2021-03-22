5bab9b4379bb7c9ed7bf728812c94b56340d3abc
Jose Solis <jsolis@wpnops.biz>
2021-03-22 17:54:59 -0600

    [FIX] - Lock ansible version and fix Centos7 repo issues

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,2 +1,10 @@
-def test_command(host):
-    assert host.command('service td-agent-bit status').rc == 0
+def pkg_is_installed(host):
+    package = host.package('td-agent-bit')
+
+    assert package.is_installed
+
+def test_service_is_running(host):
+    service = host.service('td-agent-bit')
+
+    assert service.is_running
+    assert service.is_enabled
