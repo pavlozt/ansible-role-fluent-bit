010d4b6d816b9b44d576f6f6adf00fff9a6724c2
pavlozt <pwlonw@gmail.com>
2023-03-10 15:12:18 +0300

    WIP: GH actions

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,13 +4,6 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-def test_hosts_file(host):
-    f = host.file("etc/hosts")
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
-
-
 def test_installation(host):
     assert host.package("fluent-bit").is_installed
 
